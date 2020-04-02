8cef2095342995e2b32ca70ac824d52b745a05fa
developer223 <1758875192@qq.com>
2020-04-02 11:46:54 +0800

    4.2

--- app/src/main/java/com/example/user/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/user/myapplication/Main2Activity.java
@@ -1,15 +1,24 @@
 package com.example.user.myapplication;
 
 import android.app.WallpaperManager;
+import android.content.Intent;
+import android.media.MediaCodec;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main2Activity extends AppCompatActivity implements View.OnFocusChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,16 +48,38 @@ public class Main2Activity extends AppCompatActivity {
 
 
         });
+        EditText email=(EditText) findViewById(R.id.ch4_1_email);
+        email.setOnFocusChangeListener(this);
+
+        LinearLayout linearLayout=(LinearLayout)findViewById(R.id.ch4_1_11);
+        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                float x=motionEvent.getX();
+                float y=motionEvent.getY();
+                TextView textView=(TextView) findViewById(R.id.ch4_1_tv);
+                textView.setText("x:"+x+",y:"+y);
+                return true;
+            }
+        });
+
+
     }
     public void onFocusChange(View view,boolean b){
         //判断邮箱的合法性
         EditText editText=(EditText) view;
         if(!b){
-            String content=editText.getText().toString();//得到edit的内容
-
-
-
-
+            String content=editText.getText().toString();//得到EditText的内容
+            //判断邮箱地址的正则表达式
+            String regEx1 = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+            Pattern pattern= Pattern.compile(regEx1);
+            Matcher matcher=pattern.matcher(content);
+            TextView textView=(TextView)findViewById(R.id.ch4_1_tv);
+            if(matcher.matches()){
+                textView.setText("");
+            }else{
+                textView.setText("email invalidate");
+            }
         }
     }
     //2、实现事件监听类
@@ -58,6 +89,11 @@ public class Main2Activity extends AppCompatActivity {
             Log.i(Main2Activity.class.toString(),"button click");
         }
     }
+        public  void startMain(View view){
+            //界面跳转
+           //Intent intent=new Intent(this.Main2Activity.class);
+           //startActionMode((ActionMode.Callback) intent);
 
+        }
 
 }
